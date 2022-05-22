15cb096c400cc3909fda45176e0a57ee24bbe0a5
Tasneem safia <76720686+Tasneem-Abu-Safia@users.noreply.github.com>
2022-05-23 02:08:23 +0300

    Set up Sanity Database

--- client/lib/client.js
+++ client/lib/client.js
@@ -1,9 +1,12 @@
 import sanityClient from '@sanity/client'
 
 export const client = sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: "t6a4d9yq",
   dataset: 'production',
   apiVersion: 'v1',
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   useCdn: false,
 })
+
+
+
